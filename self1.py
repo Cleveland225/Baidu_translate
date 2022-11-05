c5ffb04d2cf4a2fe0253b5b1566112bd554b9e43
Snowchord <Cleveland225@163.com>
2022-11-06 02:07:18 +0800

    jp2zh now

---
+++ self1.py
@@ -0,0 +1,49 @@
+import re
+import execjs
+import requests
+from urllib import parse
+
+session = requests.session()
+index_url = 'https://fanyi.baidu.com/'
+lang_url = 'https://fanyi.baidu.com/langdetect'
+translate_api = 'https://fanyi.baidu.com/v2transapi'
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.37'
+        }
+
+query = input('请输入要翻译的文字：')
+session.get(url=index_url, headers=headers)
+# print(session.cookies.get_dict())
+response_index = session.get(url=index_url, headers=headers)
+# print(response_index)
+token = re.findall(r"token: '([0-9a-z]+)'", response_index.text)[0]
+gtk = re.findall(r'gtk = "(.*?)"', response_index.text)[0]
+print(token)
+print(gtk)
+response_lang = session.post(url=lang_url, headers=headers, data={'query': query})
+lang = response_lang.json()['lan']
+print(lang)
+
+with open('self1.js', 'r', encoding='utf-8') as f:
+    baidu_js = f.read()
+sign = execjs.compile(baidu_js).call('e', query)
+print(sign)
+translate_url = 'https://fanyi.baidu.com/#%s/zh/%s' % (lang, parse.quote(query))
+acs_token = execjs.compile(baidu_js).call('ascToken', translate_url)
+
+data={
+	"from":lang,
+	"to": "zh",
+	"query": query,
+	"transtype": "translang",
+	"simple_means_flag": "3",
+	"sign": sign,
+	"token": token,
+	"domain": "common",
+}
+
+headers["Acs-Token"] = acs_token
+
+response = session.post(url=translate_api, headers=headers, data=data)
+result = response.json()['trans_result']['data'][0]['dst']
+print(result)
